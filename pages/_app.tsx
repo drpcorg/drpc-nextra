63ce056fcad7c54652da655cf1c57014099915f9
Konstantin Ryzhov <theaklair@gmail.com>
2024-05-20 14:46:33 +0500

    _app.mdx to _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import { createTheme, MantineProvider } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
+import { AppProps } from "next/app";
+
+import "@mantine/notifications/styles.css";
+import "@mantine/core/styles.css";
+
+const theme = createTheme({});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <MantineProvider theme={theme}>
+      <Notifications />
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
+}
