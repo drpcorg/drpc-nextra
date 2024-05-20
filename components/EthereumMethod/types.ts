a3cdc4f1a9a696d31ca307a1aae4d2f322db4974
Konstantin Ryzhov <theaklair@gmail.com>
2024-05-20 14:47:35 +0500

    Create types.ts

---
+++ components/EthereumMethod/types.ts
@@ -0,0 +1,14 @@
+export type TParamType =
+  | "string"
+  | "number"
+  | "integer"
+  | "boolean"
+  | "array"
+  | "object"
+  | "array_of_objects"
+  | "array_of_strings";
+
+export type CodeSnippetObject = {
+  language: "js" | "shell" | "node" | "go" | "python" | "rust";
+  code: string;
+};
