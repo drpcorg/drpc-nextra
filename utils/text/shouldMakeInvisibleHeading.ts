119576bc627aeb37f4db969598cca5c840fb18c8
Konstantin Ryzhov <theaklair@gmail.com>
2024-10-04 16:25:10 +0500

    Updated shouldMakeInvisibleHeading, fixed some Solana URLs

--- utils/text/shouldMakeInvisibleHeading.ts
+++ utils/text/shouldMakeInvisibleHeading.ts
@@ -4,9 +4,23 @@ const METHOD_DOCS_URL_BASES = [
   "/ethereum-api",
   "/optimism-api",
   "/solana-api",
-  "/polygon-api,",
-  "/avalanche-api,",
-  "/mantle-api,"
+  "/polygon-api",
+  "/avalanche-api",
+  "/mantle-api",
+];
+
+const METHOD_DOCS_URL_CHAPTERS = [
+  "blocksinfo",
+  "transactionsinfo",
+  "debugandtrace",
+  "accountinfo",
+  "eventlogs",
+  "chaininfo",
+  "executingtransactions",
+  "gasestimation",
+  "gettinguncles",
+  "web3",
+  "subscriptions",
 ];
 
 export function shouldMakeInvisibleHeading(value: string) {
@@ -16,6 +30,11 @@ export function shouldMakeInvisibleHeading(value: string) {
   const isDocsInfoPage = METHOD_DOCS_URL_BASES.some(
     (base) => value === base || value === `${base}/`
   );
+  const isDocsChapterPage = METHOD_DOCS_URL_BASES.some((base) =>
+    METHOD_DOCS_URL_CHAPTERS.some((chapter) =>
+      value.startsWith(`${base}/${chapter}`)
+    )
+  );
 
-  return isDocsPage && !isDocsInfoPage;
+  return isDocsPage && !isDocsInfoPage && !isDocsChapterPage;
 }
